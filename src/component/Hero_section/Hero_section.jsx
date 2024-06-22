925d9d45ae26c34a851abbf3dba248f6ace146c5
ShaYri12 <sharjeelb60@gmail.com>
2024-06-23 02:41:14 +0500

    fix the styling

--- src/component/Hero_section/Hero_section.jsx
+++ src/component/Hero_section/Hero_section.jsx
@@ -12,26 +12,23 @@ function Hero_section() {
               <span className="h1-other">Choose the best...</span>
             </h1>
             <p className="hero-para">Company reviews, Course review, Internship</p>
-            
-            <div className="input-group has-search mb-3">
-              
-                <span className="fa fa-search form-control-icon"></span>
-                <input type="text" className="form-control rounded-start" placeholder="Search by course" aria-label="Search" aria-describedby="basic-addon1"/>
-                
-                <button className="btn btn-course rounded-end bg-white dropdown-toggle ps-4"  data-bs-toggle="dropdown" aria-expanded="false">Course</button>
-                <ul className="dropdown-menu dropdown-menu-end me-3">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Separated link</a></li>
-                </ul>
-              
-              <div className="btn-search-box ms-3">
-                <button className="btn-search btn btn-lg btn-success">Search</button>
-              </div>
+            <div className="search-area d-flex flex-row gap-2">
+                <div className="input-group has-search">
+                  <span className="fa fa-search form-control-icon"></span>
+                  <input type="text" className="form-control rounded-start" placeholder="Search by course" aria-label="Search" aria-describedby="basic-addon1"/>
+                  <button className="btn btn-course border border-y-2 rounded-end bg-white dropdown-toggle ps-4"  data-bs-toggle="dropdown" aria-expanded="false">Course</button>
+                  <ul className="dropdown-menu dropdown-menu-end me-3">
+                    <li><a className="dropdown-item" href="#">Action</a></li>
+                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                    <li><hr className="dropdown-divider"/></li>
+                    <li><a className="dropdown-item" href="#">Separated link</a></li>
+                  </ul>
+                </div>
+                <div className="btn-search-box">
+                  <button className="btn-search btn btn-lg mt-0 btn-success">Search</button>
+                </div>
             </div>
-            
           </div>
           <div className="col-12 col-md-6 col-lg-6 d-flex justify-content-end align-items-center person-box">
             <div className="hero-person img-fluid d-flex justify-content-start align-items-end "></div>  
